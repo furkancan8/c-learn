a46ada6b6f27096be7dcc2e75798d6bd63c6f80d
Fc <furkan_can45@hotmail.com>
2026-03-17 22:02:54 +0300

    Proje dosyası ekle.

---
+++ c++learn/c++learn.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	//Asal sayıları ekrana yazdıran program
+    //int LimitAsal = 17;
+    //for (int i = 2; i <= LimitAsal; i++) {
+    //    bool asalMi = true; 
+
+    //    for (int j = 2; j * j <= i; j++) {
+    //      if (i % j == 0) {
+    //          asalMi = false; 
+    //          break;          
+    //      }
+    //    }
+    S
+    //    if (asalMi) {
+    //        cout << i << " asal sayidir\n";
+    //    }
+    //}
+    // return 0;
+
+    //---------------------------------------
+     
+	//Faktöriyel hesaplayan program
+	//int sayi,toplam=1;
+	//cout << "Faktoriyelini hesaplamak istediginiz sayiyi giriniz: ";
+	//cin >> sayi;
+    //for (int i = 1; i <= sayi; i++)
+    //{
+	//	toplam *= i;
+    //}
+	//cout << "Faktoriyel: " << toplam;
+     
+    //---------------------------------------
+     
+    //Tersten Faktöriyel hesaplayan program 
+    //  int sayi, toplam;
+    //  cout << "Faktoriyelini hesaplamak istediginiz sayiyi giriniz: ";
+    //  cin >> sayi;
+    //  toplam = sayi;
+    //  for (int i = sayi - 1 ; i > 1 ; i--)
+    //  {
+    //	  toplam *= i;
+    //  }
+    //  cout << "Faktoriyel: " << toplam;
+
+    //---------------------------------------
+	
+    //En büyük ve en küçük sayıyı bulan program
+	//int enBuyuk, enKucuk;
+    //int sayilar[5] = {23,76,6,5,32};
+    //enKucuk = sayilar[0];
+    //enBuyuk = sayilar[0];
+    //for (int i = 1; i < size(sayilar);i++)
+    //{
+    //    if (enKucuk > sayilar[i])
+    //    {
+    //        enKucuk = sayilar[i];
+    //    }
+    //    if (enBuyuk < sayilar[i])
+    //    {
+	//		enBuyuk = sayilar[i];
+    //    }
+    //}
+    //cout << "En Buyuk sayi: " << enBuyuk;
+    //cout << "En Kucuk sayi: " << enKucuk;
+}
